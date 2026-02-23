b0c4a9f5a299e2e8b80d911456bca3587b1aee75
Pranathi C <pranathic15@gmail.com>
2026-02-23 22:27:10 +0530

    Added MyDetails Program
    This program prints my name, college and branch using java

---
+++ MyDetails.java
@@ -0,0 +1,10 @@
+public class MyDetails {
+    public static void main(String[] args) {
+        String name = "Pranathi";
+        String college = "Dr.T.Thimmaiah Institute of Technology";
+        String branch = "Computer Science";
+        System.out.println(name);
+        System.out.println(college);
+        System.out.println(branch);
+    }
+}
