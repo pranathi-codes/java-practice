7727dc72d7d57c1bfa0456751a4d9fcb11038c0a
Pranathi C <pranathic15@gmail.com>
2026-02-24 23:02:10 +0530

    Add Two numbers program

---
+++ Day03_AddTwonumbers.java
@@ -0,0 +1,8 @@
+public class Day03_AddTwonumbers {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int res = a + b;
+        System.out.println(res);
+    }
+}
