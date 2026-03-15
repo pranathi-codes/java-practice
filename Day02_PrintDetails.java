eac0f39d8d488ace6ce559cc5f4507422adaad6a
Pranathi C <pranathic15@gmail.com>
2026-03-15 19:12:51 +0530

    Day 2: Java variables program

---
+++ Day02_PrintDetails.java
@@ -0,0 +1,13 @@
+public class Day02_PrintDetails {
+
+    public static void main(String[] args) {
+        String name = "Pranathi";
+        int age = 19;
+        String college = "Dr.TTIT";
+
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("College : "+college);
+
+    }
+}
