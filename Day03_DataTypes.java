1074fa104967871afd53306a425095b6ea5331e3
Pranathi C <pranathic15@gmail.com>
2026-03-16 20:42:48 +0530

    Day03_DataTypes.java

---
+++ Day03_DataTypes.java
@@ -0,0 +1,30 @@
+public class Day03_DataTypes {
+
+    public static void main(String[] args) {
+        byte age = 19;
+        short year = 2025;
+        int num = 500;
+        char grade ='A';
+        float temp = 34.59f;
+        double price = 99.99;
+        long mob = 9876543210L;
+        boolean isStudent = true;
+
+        System.out.println("Age : "+age);
+        System.out.println("Year : "+year);
+        System.out.println("Num : "+num);
+        System.out.println("Grade : "+grade);
+        System.out.println("Temp : "+temp);
+        System.out.println("Price : "+price);
+        System.out.println("Mobile : "+mob);
+        System.out.println("IsStudent : "+isStudent);
+
+    }
+}
+
+
+
+
+
+
+
