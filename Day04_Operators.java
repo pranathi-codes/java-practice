f292030ae1b3a48ee794b5a4fd3d1d329a197247
Pranathi C <pranathic15@gmail.com>
2026-03-17 20:04:54 +0530

    Java program for arithmetic operators

---
+++ Day04_Operators.java
@@ -0,0 +1,18 @@
+public class Day04_Operators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=7;
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a*b;
+        int div = a/b;
+        int mod = a%b;
+        System.out.println("Addition : "+sum);
+        System.out.println("Subtraction : "+sub);
+        System.out.println("Multiplication : "+mul);
+        System.out.println("Division : "+div);
+        System.out.println("Modulus : "+mod);
+    }
+}
+
+
