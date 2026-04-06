85b0312c98662ce12021b1d5319c3485d06f3adc
Pranathi C <pranathic15@gmail.com>
2026-04-06 22:52:59 +0530

    Implemented basic function

---
+++ Day24_FunctionBasic.java
@@ -0,0 +1,9 @@
+public class Day24_FunctionBasic {
+    public static void main(String[] args) {
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Hello world!");
+    }
+}
